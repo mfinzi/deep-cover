0ee5f254270d58bbc354fa43b7eb62d353b97aea
Marc Finzi <mfinzi@hmc.edu>
2019-10-08 15:11:00 -0400

    removed cuda for cpu only instance

--- stegapp.py
+++ stegapp.py
@@ -7,17 +7,16 @@ from encode import encode_long_text, decode_long_text
 def load_models():
     en2fr = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-de.single_model', tokenizer='moses', bpe='fastbpe')
     fr2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.de-en.single_model', tokenizer='moses', bpe='fastbpe')
-    en2fr.cuda();
-    fr2en.cuda();
+    #en2fr.cuda();
+    #fr2en.cuda();
     return (en2fr,fr2en)
 models = load_models()
+T = st.slider("Temperature", min_value=0.7, max_value=1.5, value=1.05, step=None, format=None)
+topk = st.slider("topk", min_value=5, max_value=200, value=25, step=5, format=None)
 phrase="Japanese researchers began studying transistors three months after they were invented at America’s Bell Labs in 1947. Japanese companies then used transistors and other electronic parts and components to produce radios, television sets, Sony Walkmans, video cassette recorders, and computers. As the yen appreciated by 60% following the 1985 Plaza Accord, Japanese companies lost competitiveness in final electronics goods and moved upstream in electronics value chains. They focused on exporting electronic parts and components and capital goods to producers of final electronics goods abroad. "
 cover_text = st.text_area("Cover Text", phrase)
 
 secret_message = st.text_area("Secret Message","secret code here: blah blah")
-T = st.slider("Temperature", min_value=0.7, max_value=1.5, value=1.05, step=None, format=None)
-topk = st.slider("topk", min_value=5, max_value=200, value=25, step=5, format=None)
-
 encoded_text, bits_encoded = encode_long_text(phrase,secret_message,models,temperature=T,sampling_topk=topk)
 st.write('Cover + Payload:')
 st.write(encoded_text)
